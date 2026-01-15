669a288e6ae7cf558adac2ec3ebd48ec11826073
bhaiyaa11 <bhavyaagrawal1011@gmail.com>
2026-01-15 14:48:17 +0530

    Remove AuthProvider and Sheader from App component
    Comment out AuthProvider and Sheader components.

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import ChatWindow from './components/chat_window.jsx'
 import Sidebar from './components/siderbar.jsx'
 // import Sheader from './components/sheader.jsx'
 import { ChatProvider } from './contexts/ChatContext.jsx'
-import { AuthProvider } from './contexts/AuthContext.jsx'
+// import { AuthProvider } from './contexts/AuthContext.jsx'
 function App() {
   return (
-    <AuthProvider>
+    // <AuthProvider>
       <ChatProvider>
         {/* <Sheader /> */}
         <div className="app-layout">
@@ -14,7 +14,7 @@ function App() {
           <ChatWindow />
         </div>
       </ChatProvider>
-    </AuthProvider>
+    // </AuthProvider>
   )
 }
 
