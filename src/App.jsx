aa9690cf8cb610b8543e4c26f39446a403396ee2
bhaiyaa11 <bhavyaagrawal1011@gmail.com>
2026-01-15 14:46:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
 import ChatWindow from './components/chat_window.jsx'
 import Sidebar from './components/siderbar.jsx'
-import Sheader from './components/sheader.jsx'
+// import Sheader from './components/sheader.jsx'
 import { ChatProvider } from './contexts/ChatContext.jsx'
 import { AuthProvider } from './contexts/AuthContext.jsx'
 function App() {
   return (
     <AuthProvider>
       <ChatProvider>
-        <Sheader />
+        {/* <Sheader /> */}
         <div className="app-layout">
           <Sidebar />
           <ChatWindow />
