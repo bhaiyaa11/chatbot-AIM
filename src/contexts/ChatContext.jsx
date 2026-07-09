d339bc1585ee62c4c53d3e380881dc3fdd730a65
bhaiyaa11 <bhavyaagrawal1011@gmail.com>
2026-07-09 20:34:56 +0530

    version5.3.2

--- src/contexts/ChatContext.jsx
+++ src/contexts/ChatContext.jsx
@@ -7,7 +7,7 @@ const ChatContext = createContext();
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const ChatProvider = ({ children }) => {
-  const { session } = useAuth();
+  const { session } = useAuth() ; 
 
   const [messages, setMessages] = useState([]);
   const [conversations, setConversations] = useState([]);
